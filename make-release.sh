8712531fe32605a2d98d682b49775c93009ec181
Allis Tauri <allista@gmail.com>
2017-01-08 00:44:39 +0300

    Made full CC a dependency, othewrise GC provides no means of storing StructureResource.

--- make-release.sh
+++ make-release.sh
@@ -5,6 +5,5 @@ cd $(dirname "$0")
 ../../PyKSPutils/make_mod_release \
 -e '*/config.xml' '*.user' '*.orig' '*.mdb' \
 'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
-'GameData/ConfigurableContainers/Parts/*' \
 'GameData/000_AT_Utils/ResourceHack.cfg' \
 -i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
