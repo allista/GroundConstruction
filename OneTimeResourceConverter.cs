506167e2610a0b8b2df726ddf6e800797fa61c0d
Allis Tauri <allista@gmail.com>
2017-01-08 13:16:48 +0300

    Added OneTimeResourceConverter to switch to new StructureResource.
    Issue #3.
    Needs testing.

---
+++ OneTimeResourceConverter.cs
@@ -0,0 +1,149 @@
+﻿//   OneTimeResourceConverter.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2017 Allis Tauri
+
+using System;
+using System.Collections.Generic;
+using AT_Utils;
+
+using UnityEngine;
+
+namespace GroundConstruction
+{
+	public class OneTimeResourceConverter : PartModule
+	{
+		[KSPField] public string ConvertFrom = "";
+		PartResourceDefinition old_res;
+
+		public override void OnStart(StartState state)
+		{
+			base.OnStart(state);
+			old_res = PartResourceLibrary.Instance.GetDefinition(ConvertFrom);
+			if(!HighLogic.LoadedSceneIsFlight || 
+			   old_res == null || old_res.name == Globals.Instance.StructureResource.name)
+				this.EnableModule(false);
+		}
+
+		Vessel to_convert = null;
+		IEnumerator<YieldInstruction> convert_vessel(Vessel vsl)
+		{
+			yield return null;
+			var GLB = Globals.Instance;
+			var ratio = old_res.density/GLB.StructureResource.density;
+			foreach(var p in vsl.Parts)
+			{
+				var res = p.Resources.Get(old_res.id);
+				if(res == null) continue;
+				var tanks = p.Modules.GetModules<ModuleSwitchableTank>();
+				var tank = tanks.Find(t => t.Resource != null && t.Resource.resourceName == old_res.name);
+				if(tank == null)
+				{
+					var new_amount = res.amount*ratio;
+					var new_max = res.maxAmount*ratio;
+					if(new_amount > new_max) new_amount = new_max;
+					p.Resources.Add(
+						GLB.StructureResource.name,
+						new_amount,
+						new_max,
+						res.flowState,
+						res.isTweakable,
+						res.hideFlow,
+						res.isVisible,
+						res.flowMode
+					);
+					p.RemoveResource(old_res.id);
+				}
+				else
+				{
+					var new_amount = tank.Resource.amount*ratio;
+					var existing_tank = tanks.Find(t => t.Resource != null && t.Resource.resourceName == GLB.StructureResource.name);
+					//if already have such tank, transfer new resource into it, as much as possible
+					if(existing_tank != null)
+					{
+						var space = existing_tank.MaxAmount-existing_tank.Amount;
+						if(space > new_amount) existing_tank.Amount = existing_tank.Amount+new_amount;
+						else
+						{ 
+							existing_tank.Amount = existing_tank.MaxAmount;
+							new_amount -= space;
+						}
+						tank.Amount = new_amount/ratio;
+					}
+					else //convert tank type and tank resource
+					{
+						var old_amount = tank.Amount;
+						if(tank.ForceSwitchResource(GLB.StructureResource.name))
+							tank.Amount = new_amount;
+						else //if failed, try to switch back and revert
+						{
+							Utils.Message("WARNING: Unable to switch the SwitchableTank in the '{0}' to the new resource.", part.name);
+							if(tank.ForceSwitchResource(res.resourceName))
+								tank.Amount = old_amount;
+							else Utils.Message("ERROR: Unable to switch the SwitchableTank in the '{0}' back to its original resource.", part.name);
+						}
+					}
+				}
+			}
+		}
+
+		[KSPEvent(guiName = "Convert Old GC Resources", guiActive = true, active = true)]
+		public void Convert() { show_window = true; }
+
+		bool show_window;
+		const float width = 150;
+		const float height = 60;
+		Rect WindowPos = new Rect((Screen.width-width)/2, Screen.height/4, width, height*4);
+		Vector2 vessels_scroll = Vector2.zero;
+		SimpleDialog warning = new SimpleDialog();
+
+		void main_window(int WindowID)
+		{
+			GUILayout.BeginVertical();
+			vessels_scroll = GUILayout.BeginScrollView(vessels_scroll, Styles.white);
+			foreach(var vsl in FlightGlobals.VesselsLoaded)
+			{
+				GUILayout.BeginHorizontal();
+				GUILayout.Label(vsl.vesselName, Styles.white, GUILayout.ExpandWidth(true));
+				if(GUILayout.Button("Convert", Styles.danger_button, GUILayout.ExpandWidth(false)))
+				{ if(to_convert != null) to_convert = vsl; }
+				GUILayout.EndHorizontal();
+			}
+			GUILayout.EndScrollView();
+			if(GUILayout.Button("Close", Styles.close_button, GUILayout.ExpandWidth(true)))
+				show_window = false;
+			GUILayout.EndVertical();
+			GUIWindowBase.TooltipsAndDragWindow(WindowPos);
+		}
+
+		void OnGUI()
+		{
+			if(Event.current.type != EventType.Layout && Event.current.type != EventType.Repaint) return;
+			if(show_window && GUIWindowBase.HUD_enabled && vessel.isActiveVessel)
+			{
+				Styles.Init();
+				WindowPos = GUILayout.Window(GetInstanceID(), 
+				                             WindowPos, main_window, part.partInfo.title,
+				                             GUILayout.Width(width),
+				                             GUILayout.Height(height)).clampToScreen();
+				if(to_convert != null)
+				{
+					warning.Show(string.Format("This will convert '{0}' resource into '{1}' by mass in every part that contains it.\n" +
+					                           "<color=red><b>This cannot be undone!</b></color>\n" +
+					                           "It is best that you <b>save the game</b> before doing this.\n" +
+					                           "Are you sure you wish to continue?", old_res.name, Globals.Instance.StructureResource.name));
+					if(warning.Result == SimpleDialog.Answer.Yes)
+					{
+						StartCoroutine(convert_vessel(to_convert));
+						to_convert = null;
+					}
+					else if(warning.Result == SimpleDialog.Answer.No)
+						to_convert = null;
+				}
+			}
+		}
+	}
+}
+
