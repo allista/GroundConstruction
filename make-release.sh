5cb7bc133a70a3691c84f9fc656ca7e0f4156419
Allis Tauri <allista@gmail.com>
2017-02-06 01:41:23 +0300

    Excluded SubmodelResizer.dll from archive.

--- make-release.sh
+++ make-release.sh
@@ -5,5 +5,6 @@ cd $(dirname "$0")
 ../../PyKSPutils/make_mod_release \
 -e '*/config.xml' '*.user' '*.orig' '*.mdb' \
 'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
+'GameData/000_AT_Utils/Plugins/SubmodelResizer.dll' \
 'GameData/000_AT_Utils/ResourceHack.cfg' \
 -i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
