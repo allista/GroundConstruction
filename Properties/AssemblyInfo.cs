8864905c542b3e0044d18024c04f9b165fe5fe35
Allis Tauri <allista@gmail.com>
2017-02-06 00:37:10 +0300

    Changed version to 1.1.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("1.1.*")]
 #else
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
 #endif
 [assembly: KSPAssembly("GroundConstruction", 1, 1)]
 
