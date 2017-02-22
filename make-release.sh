e5216168dfe5f0a8b0c990f81e92ca977083a526
Allis Tauri <allista@gmail.com>
2017-02-22 18:15:05 +0300

    Excluded ConfigurableContainers/Parts from release to conform with CKAN.

--- make-release.sh
+++ make-release.sh
@@ -7,4 +7,5 @@ cd $(dirname "$0")
 'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
 'GameData/000_AT_Utils/Plugins/SubmodelResizer.dll' \
 'GameData/000_AT_Utils/ResourceHack.cfg' \
+'GameData/ConfigurableContainers/Parts/*' \
 -i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
