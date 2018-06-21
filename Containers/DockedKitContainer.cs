018b0f406ebf9f9b9cba769e77f97c939787a37f
Allis Tauri <allista@gmail.com>
2018-06-21 16:36:07 +0300

    Moved ModuleConstructionKit to Containers; added DockedKitContainer
    Removed ModuleSpawnTest

---
+++ Containers/DockedKitContainer.cs
@@ -0,0 +1,16 @@
+﻿//   DockedKitContainer.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2018 Allis Tauri
+using System;
+namespace GroundConstruction
+{
+    public class DockedKitContainer
+    {
+        public DockedKitContainer()
+        {
+        }
+    }
+}
