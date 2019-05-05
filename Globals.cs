cab417babf02477f7e2090caf7dcf5bfdefefddd
Allis Tauri <allista@gmail.com>
2019-05-05 21:41:04 +0300

    I50 added GLB.IgnoreModules to exlude some PartModules from complexity calc.
    For that:
    
    * Added ConfigValueSet<T> abstract class that loads values of all the config
    nodes with the specified name from the database. The main logic is that of the
    former KeepResourcesIDs prop.
    * Derived ResourceIdSet and ModuleNamesSet from ConfigValueSet. Used the first
    for the KeepResourcesIDs field and the second for the IgnoreModules field.
    * Added respective .cfg with some obvious modules to exclude.
    * Implemented corresponding logic in PartKit.
    
    Closed #50

--- Globals.cs
+++ Globals.cs
@@ -10,71 +10,126 @@ using AT_Utils;
 
 namespace GroundConstruction
 {
-    public class ResourceUsageInfo : ResourceInfo
-    {
-        [Persistent] public float ComplexityWork = 0;
-        [Persistent] public float EnergyPerMass = 0;
-        [Persistent] public float WorkPerMass = 0;
-
-        public ResourceUsageInfo(string name = "") : base(name) {}
-    }
-
     public class Globals : PluginGlobals<Globals>
     {
         #region Resources
         [Persistent] public ResourceUsageInfo AssemblyResource = new ResourceUsageInfo("SpecializedParts");
         [Persistent] public ResourceUsageInfo ConstructionResource = new ResourceUsageInfo("MaterialKits");
 
-        const string RESOURCES_NODE = "GC_KIT_RESOURCES";
-        HashSet<int> keep_res_ids;
-        public HashSet<int> KeepResourcesIDs
-        {
-            get
-            {
-                if(keep_res_ids == null)
-                {
-                    keep_res_ids = new HashSet<int>();
-                    var nodes = GameDatabase.Instance.GetConfigNodes(RESOURCES_NODE);
-                    foreach(var node in nodes)
-                    {
-                        foreach(ConfigNode.Value val in node.values)
-                        {
-                            var res = PartResourceLibrary.Instance.GetDefinition(val.value);
-                            if(res != null) keep_res_ids.Add(res.id);
-                        }
-                    }
-                }
-                return keep_res_ids;
-            }
-        }
+        public ResourceIdSet KeepResourcesIDs = new ResourceIdSet("GC_KIT_RESOURCES");
         #endregion
 
-        [Persistent] public bool  UseStockAppLauncher = true;
+        public ModuleNamesSet IgnoreModules = new ModuleNamesSet("GC_IGNORE_MODULES");
+
+        [Persistent] public bool UseStockAppLauncher = true;
 
         [Persistent] public float WorkshopShutdownThreshold = 0.99f;
-        [Persistent] public float MaxDistanceToWorkshop     = 300f;
-        [Persistent] public float MinDistanceToWorkshop     = 50f;
-        [Persistent] public float MaxDistanceEfficiency     = 0.2f;
+        [Persistent] public float MaxDistanceToWorkshop = 300f;
+        [Persistent] public float MinDistanceToWorkshop = 50f;
+        [Persistent] public float MaxDistanceEfficiency = 0.2f;
 
-        [Persistent] public float VolumePerKerbal           = 3;
-        [Persistent] public float PartVolumeFactor          = 0.3f;
-        [Persistent] public float MinGenericEfficiency      = 0.05f;
-        [Persistent] public float MaxGenericEfficiency      = 0.5f;
+        [Persistent] public float VolumePerKerbal = 3;
+        [Persistent] public float PartVolumeFactor = 0.3f;
+        [Persistent] public float MinGenericEfficiency = 0.05f;
+        [Persistent] public float MaxGenericEfficiency = 0.5f;
 
         [Persistent] public float ComplexityFactor = 1e-4f;
 
         [Persistent] public float ComplexityWeight = 110;
-        [Persistent] public float MetalworkWeight  = 100;
+        [Persistent] public float MetalworkWeight = 100;
 
         [Persistent] public float VesselKitDensity = 0.5f; //t/m3
         [Persistent] public float MinKitVolume = 0.02f; //m3
 
-        [Persistent] public float DeployMaxSpeed   = 0.5f;  //m/s
-        [Persistent] public float DeployMaxAV      = 5e-6f; //(rad/s)2
+        [Persistent] public float DeployMaxSpeed = 0.5f;  //m/s
+        [Persistent] public float DeployMaxAV = 5e-6f; //(rad/s)2
         [Persistent] public float MaxDeploymentMomentum = 1;     //t*m/s
         [Persistent] public float MinDeploymentTime = 3;    //s
 
         [Persistent] public int EasingFrames = 120;
     }
-}
 
+    public class ResourceUsageInfo : ResourceInfo
+    {
+        [Persistent] public float ComplexityWork = 0;
+        [Persistent] public float EnergyPerMass = 0;
+        [Persistent] public float WorkPerMass = 0;
+
+        public ResourceUsageInfo(string name = "") : base(name) { }
+    }
+
+    public abstract class ConfigValueSet<T>
+    {
+        protected string node_name = string.Empty;
+        HashSet<T> values;
+
+        protected abstract bool transform_value(ConfigNode.Value val, out T value);
+
+        protected ConfigValueSet(string nodeName)
+        {
+            node_name = nodeName;
+        }
+
+        public HashSet<T> Values
+        {
+            get
+            {
+                if(values == null)
+                {
+                    values = new HashSet<T>();
+                    T value;
+                    var nodes = GameDatabase.Instance.GetConfigNodes(node_name);
+                    foreach(var node in nodes)
+                    {
+                        foreach(ConfigNode.Value val in node.values)
+                        {
+                            if(transform_value(val, out value))
+                                values.Add(value);
+                        }
+                    }
+                }
+                return values;
+            }
+        }
+
+        public static implicit operator HashSet<T>(ConfigValueSet<T> configValueSet)
+        => configValueSet.Values;
+    }
+
+    public class ModuleNamesSet : ConfigValueSet<string>
+    {
+        public ModuleNamesSet(string nodeName) : base(nodeName) { }
+        protected override bool transform_value(ConfigNode.Value val, out string value)
+        {
+            value = val.value;
+            return true;
+        }
+
+        public int SizeOfDifference(PartModuleList modules)
+        {
+            int size = 0;
+            for(int i = 0, modulesCount = modules.Count; i < modulesCount; i++)
+            {
+                if(!Values.Contains(modules[i].ClassName))
+                    size++;
+            }
+            return size;
+        }
+    }
+
+    public class ResourceIdSet : ConfigValueSet<int>
+    {
+        public ResourceIdSet(string nodeName) : base(nodeName) { }
+        protected override bool transform_value(ConfigNode.Value val, out int value)
+        {
+            value = 0;
+            var res = PartResourceLibrary.Instance.GetDefinition(val.value);
+            if(res != null)
+            {
+                value = res.id;
+                return true;
+            }
+            return false;
+        }
+    }
+}
