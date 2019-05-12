c6afbf925bb159d13280effa8074dfd0d89d22bf
Allis Tauri <allista@gmail.com>
2019-05-12 23:01:10 +0300

    Using AT_Utils.AppToolbar

---
+++ GCAppToolbar.cs
@@ -0,0 +1,35 @@
+﻿//   ToolbarManager.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+
+using KSP.UI.Screens;
+using AT_Utils;
+
+namespace GroundConstruction
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class GCAppToolbar : AppToolbar<GCAppToolbar>
+    {
+        protected override string TB_ICON => "GroundConstruction/Icons/toolbar-icon";
+        protected override string AL_ICON => "GroundConstruction/Icons/applauncher-icon";
+
+        protected override ApplicationLauncher.AppScenes AP_SCENES =>
+        ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW |
+            ApplicationLauncher.AppScenes.TRACKSTATION | ApplicationLauncher.AppScenes.SPACECENTER;
+
+        protected override GameScenes[] TB_SCENES =>
+        new[] { GameScenes.FLIGHT, GameScenes.TRACKSTATION, GameScenes.SPACECENTER };
+
+        protected override string button_tooltip => "Global Construction";
+
+        protected override bool ForceAppLauncher => Globals.Instance.UseStockAppLauncher;
+
+        protected override void onLeftClick()
+        {
+            GroundConstructionScenario.ToggleWindow();
+        }
+    }
+}
