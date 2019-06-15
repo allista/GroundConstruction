6e045d62914d0d5479fec4d6a576c36f2050749e
Allis Tauri <allista@gmail.com>
2019-06-15 23:30:53 +0300

    Added GC.UI subproject for uGUI scripts and assets

---
+++ GC.UI/PartTreeNode.cs
@@ -0,0 +1,68 @@
+﻿//   PartTreeNode.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2019 Allis Tauri
+using System.Collections.Generic;
+using AT_Utils.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GC.UI
+{
+    public interface IRecycleInfo
+    {
+        uint ID { get; }
+        IEnumerable<IRecycleInfo> GetChildren();
+        void Update(PartTreeNode display);
+    }
+
+    public class PartTreeNode : PanelledUI
+    {
+        IRecycleInfo partInfo;
+
+        public Toggle subnodesToggle;
+        public Text assemblyResourceInfo, constructionResourceInfo, requirementsInfo;
+        public Toggle discardExcess;
+        public Button recycleButton;
+
+        public GameObject partTreeNodePrefab;
+        public RectTransform subnodes;
+
+        public void SetPartInfo(IRecycleInfo info)
+        {
+            showSubnodes(false);
+            partInfo = info;
+            partInfo.Update(this);
+        }
+
+        void Awake()
+        {
+            subnodesToggle.onValueChanged.AddListener(showSubnodes);
+        }
+
+        void showSubnodes(bool show)
+        {
+            if(show)
+            {
+                if(partInfo != null)
+                {
+                    foreach(var childInfo in partInfo.GetChildren())
+                    {
+                        var subnode = Object.Instantiate(partTreeNodePrefab, subnodes);
+                        subnode.GetComponent<PartTreeNode>().SetPartInfo(childInfo);
+                        subnode.SetActive(true);
+                    }
+                    subnodes.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                subnodes.gameObject.SetActive(false);
+                for(var i = subnodes.childCount-1; i >= 0; i++)
+                    Object.Destroy(subnodes.GetChild(i).gameObject);
+            }
+        }
+    }
+}
---
+++ GC.UI/RecyclerUI.cs
@@ -0,0 +1,44 @@
+﻿//   RecyclerUI.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2019 Allis Tauri
+using System.Collections.Generic;
+using AT_Utils.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GC.UI
+{
+    public class RecyclerUI : ScreenBoundRect
+    {
+        public RectTransform rootPartsContainer;
+        public ToggleGroup rootPartToggles;
+        public GameObject partTreeNodePrefab;
+
+        readonly Dictionary<uint, PartTreeNode> rootNodes = new Dictionary<uint, PartTreeNode>();
+
+        public void AddRootPart(IRecycleInfo rootPartInfo)
+        {
+            if(!rootNodes.ContainsKey(rootPartInfo.ID))
+            {
+                var newNodeObj = Object.Instantiate(partTreeNodePrefab, rootPartsContainer);
+                var newNode = newNodeObj.GetComponent<PartTreeNode>();
+                newNode.SetPartInfo(rootPartInfo);
+                newNode.subnodesToggle.group = rootPartToggles;
+                newNodeObj.SetActive(true);
+            }
+        }
+
+        public void DeleteRootPart(IRecycleInfo rootPartInfo)
+        {
+            PartTreeNode node;
+            if(rootNodes.TryGetValue(rootPartInfo.ID, out node))
+            {
+                node.gameObject.SetActive(false);
+                Object.Destroy(node.gameObject);
+            }
+        }
+    }
+}
