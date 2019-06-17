d3a2a16850cbae6d05dbee9a2232bc65d1cbdaf1
Allis Tauri <allista@gmail.com>
2019-06-17 11:07:40 +0300

    AssemblyVersion: 2.4.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("2.4.*")]
 #else
-[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.1")]
 #endif
 [assembly: KSPAssembly("GroundConstruction", 2, 4)]
 
