037f3fd3b1b8a72b001652713ca382069f0d9507
Allis Tauri <allista@gmail.com>
2020-04-26 16:41:44 +0300

    Changed Min/Max KSP versions to 1.9.0 - 1.9.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ namespace GroundConstruction
     {
         public ModInfo()
         {
-            MinKSPVersion = new Version(1, 8, 1);
-            MaxKSPVersion = new Version(1, 8, 1);
+            MinKSPVersion = new Version(1, 9, 0);
+            MaxKSPVersion = new Version(1, 9, 1);
 
             VersionURL = "https://raw.githubusercontent.com/allista/GroundConstruction/master/GameData/GroundConstruction/GroundConstruction.version";
             UpgradeURL = "https://spacedock.info/mod/1123/Ground%20Construction";
