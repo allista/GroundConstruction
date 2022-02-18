df525e84dd9758f0a1b0bf8c6838af6085763635
Allis Tauri <allista@gmail.com>
2022-02-18 18:38:47 +0300

    Fixed common warnings in make-release.sh
    Also auto-formatted it with PyCharm formatter

--- make-release.sh
+++ make-release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd $(dirname "$0")
+CWD=$(dirname "$0")
+cd "${CWD}" || exit
 
 make_mod_release \
--e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.pdb' \
-'*/System.*.dll' '*/Mono.*.dll' '*/Unity*.dll' \
-'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
-'GameData/000_AT_Utils/Plugins/SubmodelResizer.dll' \
-'GameData/000_AT_Utils/ResourceHack.cfg' \
-'GameData/ConfigurableContainers/Parts/*' \
--i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
+    -e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.pdb' \
+    '*/System.*.dll' '*/Mono.*.dll' '*/Unity*.dll' \
+    'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
+    'GameData/000_AT_Utils/Plugins/SubmodelResizer.dll' \
+    'GameData/000_AT_Utils/ResourceHack.cfg' \
+    'GameData/ConfigurableContainers/Parts/*' \
+    -i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
